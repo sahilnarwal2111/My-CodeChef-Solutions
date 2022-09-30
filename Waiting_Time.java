ed1a30fd3a13f1a06187f5ccdf245ccda1352e10
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-01 02:49:40 +0530

    Add files via upload

---
+++ Waiting_Time.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Waiting_Time {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < t; i++) {
+            String str = sc.nextLine();
+            String[] xyz = str.split(" ");
+            int x = Integer.parseInt(xyz[0]);
+            int y = Integer.parseInt(xyz[1]);
+            int diff = x*7 - y;
+            System.out.println(diff);
+        }
+    }
+}

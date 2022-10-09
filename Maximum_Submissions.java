e31ef116c13a7955c90dbb96f553bb24c091caef
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-09 23:24:23 +0530

    day-11

---
+++ Maximum_Submissions.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Maximum_Submissions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            System.out.println(n*2);
+        }
+    }
+}

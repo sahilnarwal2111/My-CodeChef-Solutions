2f410bd3d97c6a75db7e7186fb16046492a7e3af
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-09 23:25:07 +0530

    day-11

---
+++ AI_Analysing_Code.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AI_Analysing_Code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        if(t>1000) System.out.println("no");
+        else System.out.println("yes");
+    }
+}

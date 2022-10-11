bd1255eb0ddc69d6f5bbb0ffbc7e3e869b4ce931
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-11 23:44:50 +0530

    day-13

---
+++ Black_cells_in_a_chessboard.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Black_cells_in_a_chessboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        n = (n*n)/2;
+        System.out.println(n);
+    }
+}

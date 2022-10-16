fa03773693c358adf9882d375b05b1a318c6fabf
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-16 23:09:24 +0530

    day-18

--- End_Sorted.java
+++ End_Sorted.java
@@ -10,18 +10,7 @@ public class End_Sorted {
             for(int i = 0; i<n;i++){
                 arr[i] = sc.nextInt();
             }
-            int count = 0;
-            while(arr[0] != 1&& arr[n-1]!=n){
-                int i = 0;
-                for(i = 0; i<n-1; i++){
-                    int temp;
-                    temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
-                }
-                count++;
-            }
-            System.out.println(count);
+
         }
 
     }

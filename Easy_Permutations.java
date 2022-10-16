3a64588661daf976e0350bee86cffc2e709df72d
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-16 23:10:17 +0530

    day-18

---
+++ Easy_Permutations.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Easy_Permutations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            for(int i = n; i>0;i--){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}

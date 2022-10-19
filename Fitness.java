c669f127920031f8bb9c2641373d2059660c8215
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-19 22:02:17 +0530

    day-19

---
+++ Fitness.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Fitness {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            System.out.println(x*10);
+        }
+    }
+}

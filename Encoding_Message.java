e69574e9187723868819c830491e6900a92c8ee1
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-23 23:47:52 +0530

    day-23

--- Encoding_Message.java
+++ Encoding_Message.java
@@ -1,34 +1,37 @@
-import java.util.Scanner;
-import java.util.Arrays;
-public class Encoding_Message {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-->0){
-            int n = sc.nextInt();
-            String str = sc.next();
-            String[] arr = str.split("");
-            int index = n/2 ;
-            for(int i = 0; i < index; i++){
-                String temp = null;
-                temp = arr[2*i];
-                arr[2*i] = arr[2*i + 1];
-                arr[2*i + 1] = temp;
-            }
-            str = Arrays.toString(arr);
-            StringBuilder sb = new StringBuilder();
-            for(String element: arr){
-                sb.append(element);
-            }
-            str = sb.toString();
-            char [] ch = str.toCharArray();
-            for(int i = 0; i< n; i++) {
-                int x = ch[i];
-                ch[i] = (char) (219-x);
-            }
-            for(int i = 0; i<n; i++) System.out.print(ch[i]);
-
-        }
-    }
-
-}
+//import java.util.Scanner;
+//import java.util.Arrays;
+//public class Encoding_Message {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        while(t-->0){
+//            int n = sc.nextInt();
+//            String str = sc.next();
+//            String [] s = str.split("");
+//            for(int i =0; i<n; i++){
+//                String t;
+//                if(n%2!=0) {
+//                    if (i == n - 1) s[i] = s[i];
+//
+//                    else {
+//                        t = s[i];
+//                        s[i] = s[i + 1];
+//                        s[i + 1] = t;
+//                    }
+//                }
+//                else {
+//                    t = s[i];
+//                    s[i] = s[i+1];
+//                    s[i+1] = t;
+//                }
+//
+//            }
+//
+//        for(int i =0; i<n; i++){
+//            if(s[i] == "z") s[i] = "a";
+////            else s[i] = char((int)s[i] + 2*(110-(int)s[i])-1)
+//        }
+//        }
+//    }
+//
+//}

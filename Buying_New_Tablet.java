09728f01d22a65e13cf2b5cce7be52cf5ecd2289
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-24 22:32:46 +0530

    day-23

---
+++ Buying_New_Tablet.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Buying_New_Tablet {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int tc = sn.nextInt();
+        while(tc-->0){
+            int n = sn.nextInt();
+            int b = sn.nextInt();
+            int [][] arr = new int[n][2];
+            for(int i = 0; i< n; i++){
+                //width*height
+                arr[i][0] = sn.nextInt() * sn.nextInt();
+                //
+                //price
+                arr[i][1] = sn.nextInt();
+            }
+            int flag = 0;
+            int max_area =0;
+            for(int i = 0 ; i<n; i++){
+                if(arr[i][1]<= b){
+                    if(arr[i][0]>=max_area){
+                        max_area = arr[i][0];
+                    }
+                }
+                else flag = 1;
+            }
+            if(flag==1) System.out.println("no tablet");
+            else System.out.println(max_area);
+
+//            int x = 1000000;
+        }
+    }
+}

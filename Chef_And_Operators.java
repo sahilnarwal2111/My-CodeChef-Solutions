14a64b23cb14a38b16a4a57a1efa50e0ac6d469d
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-25 21:30:08 +0530

    day-26

---
+++ Chef_And_Operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Chef_And_Operators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a>b) System.out.println(">");
+            else if(a<b) System.out.println("<");
+            else System.out.println("=");
+        }
+    }
+}

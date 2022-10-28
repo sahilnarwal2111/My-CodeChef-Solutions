73bf31d49a032d4d44beed699291c9c725fd246a
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-28 23:41:40 +0530

    day-28

---
+++ Akash_and_Grid.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Akash_and_Grid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int diff = Math.abs(x-y);
+            if(diff%2 == 0) System.out.println(0);
+            else System.out.println(1);
+        }
+    }
+}

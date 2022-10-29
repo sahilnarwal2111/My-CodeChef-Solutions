a176dc82e750a4d9204bed37bcb6afecd92e9fd6
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-29 23:57:47 +0530

    day-29

---
+++ Valentine_is_coming.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Valentine_is_coming {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            System.out.println(sc.nextInt()/sc.nextInt());
+        }
+    }
+}

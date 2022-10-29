7e4a97a830a176edb2e8ab1a6cf2e80210085cfd
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-29 23:56:59 +0530

    day-29

---
+++ X_Jumps.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class X_Jumps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x =sc.nextInt();
+            int y =sc.nextInt();
+            System.out.println((x/y) + x%y);
+        }
+    }
+}

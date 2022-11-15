eb7abeaf6261af794437450fdae14c2e7c42a965
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-11-15 18:43:10 +0530

    day-2.1

---
+++ Weights.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Weights {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0){
+            int w = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z= sc.nextInt();
+
+            if(x==w||y==w||z==w|| x+y==w || y+z==w||x+z==w||x+y+w==z)
+                System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}

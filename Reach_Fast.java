940adeb1ed81b7cb6ab209d219d8105051b271ad
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-12-15 23:49:27 +0530

    day-2.1

---
+++ Reach_Fast.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Reach_Fast {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+        int t =  Integer.parseInt(br.readLine());
+        while(t-->0){
+            
+        }
+
+    }
+}
